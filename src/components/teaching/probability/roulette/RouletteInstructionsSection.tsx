403c20db4c5ee2e1a6f835c7f917461cd8c5b16d
gandalfgauss <hallidaysantos@gmail.com>
2026-03-07 13:49:16 -0300

    Alteração nas instruções da atividade da roleta

--- src/components/teaching/probability/roulette/RouletteInstructionsSection.tsx
+++ src/components/teaching/probability/roulette/RouletteInstructionsSection.tsx
@@ -31,7 +31,7 @@ export function RouletteInstructionsSection() {
                 </p>
 
                 <List>
-                  <li><strong>Etapa 1 - Probabilidade em um Espaço Amostral Equiprovável:</strong> O disco é dividido em setores iguais com cores diferentes. Você identificará o experimento aleatório, o espaço amostral e calculará probabilidades baseando-se na simetria.</li>
+                  <li><strong>Etapa 1 - Probabilidade em um Espaço Amostral Equiprovável:</strong> O discos é dividido em setores iguais com cores diferentes. Você identificará o experimento aleatório, o espaço amostral e calculará probabilidades baseando-se na simetria.</li>
                   <li><strong>Etapa 2 - Probabilidade em um Espaço Amostral Não Equiprovável por área:</strong> O disco possui setores de tamanhos diferentes. Você aprenderá que a probabilidade de cada cor depende da medida do ângulo central do setor.</li>
                   <li><strong>Etapa 3 - Probabilidade em um espaço Amostral Não Equiprovável por cores:</strong> O disco tem setores iguais, mas algumas cores se repetem. Você calculará probabilidades com base na frequência de cada cor.</li>
                 </List>
